ca6c2a551715923f48c91996931f1f6841c99e35
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-01-11 20:01:43 -0300

    Update

---
+++ spring-web-mvc/src/main/java/com/guilherme/springwebmvc/controller/JediController.java
@@ -0,0 +1,15 @@
+package com.guilherme.springwebmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class JediController {
+
+    @GetMapping("/jedi")
+    public String jedi(){
+        return  "jedi";
+    }
+}
+ 
