3a6e592023c6cd0958c1d40fe363720d3de691d1
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-01-12 20:05:25 -0300

    Refactor Get Jedi Method

--- spring-web-mvc/src/main/java/com/guilherme/springwebmvc/controller/JediController.java
+++ spring-web-mvc/src/main/java/com/guilherme/springwebmvc/controller/JediController.java
@@ -1,14 +1,23 @@
 package com.guilherme.springwebmvc.controller;
 
+import com.guilherme.springwebmvc.model.Jedi;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 
 @Controller
 public class JediController {
 
     @GetMapping("/jedi")
-    public String jedi(){
-        return  "jedi";
+    public ModelAndView jedi(){
+        final ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("jedi");
+
+        final Jedi luke = new Jedi("Luke", "SkyWalker");
+        modelAndView.addObject("allJedi", List.of(luke));
+        return  modelAndView;
     }
 }
