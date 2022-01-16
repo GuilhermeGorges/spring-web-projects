1849848d0a2a237b138f33a4070d8be0ac20a24f
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-01-16 18:47:15 -0300

    Develop Jedi Repository

--- spring-rest-completo/src/main/java/com/guilherme/springrestcompleto/repository/JediRepository.java
+++ spring-rest-completo/src/main/java/com/guilherme/springrestcompleto/repository/JediRepository.java
@@ -1,4 +1,14 @@
 package com.guilherme.springrestcompleto.repository;
 
-public class JediRepository {
+import java.util.List;
+
+import com.guilherme.springrestcompleto.model.Jedi;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JediRepository extends JpaRepository<Jedi, Long> {
+
+    List<Jedi> findByNameContainingIgnoreCase(final String name);
+
 }
