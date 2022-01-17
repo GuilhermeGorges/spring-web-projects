e1c9314eb46e8909d29a1eadf8f8f66c5fcfff48
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-01-17 19:29:31 -0300

    Develop a new layer service | Refactor all Rest Controller Methods

--- spring-rest-completo/src/main/java/com/guilherme/springrestcompleto/rest/JediResource.java
+++ spring-rest-completo/src/main/java/com/guilherme/springrestcompleto/rest/JediResource.java
@@ -1,8 +1,7 @@
 package com.guilherme.springrestcompleto.rest;
 
-import com.guilherme.springrestcompleto.exception.JediNotFoundException;
 import com.guilherme.springrestcompleto.model.Jedi;
-import com.guilherme.springrestcompleto.repository.JediRepository;
+import com.guilherme.springrestcompleto.service.JediService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,59 +9,39 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class JediResource {
 
     @Autowired
-    private JediRepository repository;
+    private JediService jediService;
 
     @GetMapping("/api/jedi")
     public List<Jedi> getAllJedi(){
-        return repository.findAll();
+        return jediService.findAll();
     }
 
     @GetMapping("/api/jedi/{id}")
     public ResponseEntity<Jedi> getJediByID(@PathVariable("id") Long id){
-        final Optional<Jedi> jedi = repository.findById(id);
+        final Jedi jedi = jediService.findById(id);
 
-        if(jedi.isPresent()){
-            return ResponseEntity.ok(jedi.get());
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return ResponseEntity.ok(jedi);
     }
 
     @PostMapping("/api/jedi")
     @ResponseStatus(HttpStatus.CREATED)
     public Jedi createJedi(@Valid @RequestBody Jedi jedi){
-        return repository.save(jedi);
+        return jediService.save(jedi);
     }
 
     @PutMapping("/api/jedi/{id}")
     public ResponseEntity<Jedi> updateJedi(@PathVariable("id") Long id, @Valid @RequestBody Jedi dto){
-        final Optional<Jedi> optionalJedi = repository.findById(id);
-        final Jedi jedi;
-
-        if(optionalJedi.isPresent()){
-            jedi = optionalJedi.get();
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-        jedi.setName(dto.getName());
-        jedi.setLastName(dto.getLastName());
-        return ResponseEntity.ok(repository.save(jedi));
+        return ResponseEntity.ok(jediService.update(id, dto));
     }
 
     @DeleteMapping("/api/jedi/{id}")
-    public ResponseEntity delete(@PathVariable("id") Long id){
-        final Optional<Jedi> jedi = repository.findById(id);
-        if(jedi.isPresent()){
-            repository.delete(jedi.get());
-            return ResponseEntity.noContent().build();
-        } else {
-        return ResponseEntity.notFound().build();
-        }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") Long id){
+        jediService.delete(id);
     }
 }
--- spring-rest-completo/src/main/java/com/guilherme/springrestcompleto/service/JediService.java
+++ spring-rest-completo/src/main/java/com/guilherme/springrestcompleto/service/JediService.java
@@ -1,4 +1,57 @@
 package com.guilherme.springrestcompleto.service;
 
+import com.guilherme.springrestcompleto.controller.JediController;
+import com.guilherme.springrestcompleto.exception.JediNotFoundException;
+import com.guilherme.springrestcompleto.model.Jedi;
+import com.guilherme.springrestcompleto.repository.JediRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class JediService {
+
+    @Autowired
+    private JediRepository jediRepository;
+
+    public List<Jedi> findAll() {
+        return jediRepository.findAll();
+    }
+
+    public Jedi findById(final Long id) {
+        final Optional<Jedi> jedi = jediRepository.findById(id);
+
+
+        if(jedi.isPresent()){
+            return jedi.get();
+        } else {
+            throw new JediNotFoundException();
+        }
+    }
+
+    public Jedi save(final Jedi jedi) {
+        return jediRepository.save(jedi);
+    }
+
+    public Jedi update(final Long id,final Jedi dto) {
+        final Optional<Jedi> optionalJedi = jediRepository.findById(id);
+        final Jedi jedi;
+
+        if(optionalJedi.isPresent()){
+            jedi = optionalJedi.get();
+        } else {
+            throw new JediNotFoundException();
+        }
+        jedi.setName(dto.getName());
+        jedi.setLastName(dto.getLastName());
+        return jediRepository.save(jedi);
+    }
+
+    public void delete(final Long id) {
+        final Jedi jedi = findById(id);
+        jediRepository.delete(jedi);
+    }
 }
