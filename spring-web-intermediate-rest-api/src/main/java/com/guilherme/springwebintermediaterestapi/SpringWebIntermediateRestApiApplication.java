07363301804b8ae332862ad48a8e0207ad7e2838
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-01-18 17:53:31 -0300

    First Intermediate Rest Api Update

---
+++ spring-web-intermediate-rest-api/src/main/java/com/guilherme/springwebintermediaterestapi/SpringWebIntermediateRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.springwebintermediaterestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebIntermediateRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebIntermediateRestApiApplication.class, args);
+	}
+
+}
---
+++ spring-web-intermediate-rest-api/src/test/java/com/guilherme/springwebintermediaterestapi/SpringWebIntermediateRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.springwebintermediaterestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebIntermediateRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
