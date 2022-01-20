545e781003f4ef05197399bf0736298de8957723
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-01-20 19:25:34 -0300

    Develop Soldier Controller Layer

--- spring-api-hateoas/src/main/java/com/guilherme/springapihateoas/controller/SoldierController.java
+++ spring-api-hateoas/src/main/java/com/guilherme/springapihateoas/controller/SoldierController.java
@@ -1,4 +1,58 @@
 package com.guilherme.springapihateoas.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.guilherme.springapihateoas.controller.request.SoldierEditRequest;
+import com.guilherme.springapihateoas.controller.response.SoldierResponse;
+import com.guilherme.springapihateoas.dto.Soldier;
+import com.guilherme.springapihateoas.service.SoldierService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/v1/soldier")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class SoldierController {
+
+    private SoldierService soldierService;
+    private ObjectMapper objectMapper;
+
+    @GetMapping
+    public ResponseEntity<List<SoldierResponse>> listAllSoldier() {
+        List<SoldierResponse> soldierResponseList = soldierService.listAllSoldiers().stream()
+                .map(it -> objectMapper.convertValue(it, SoldierResponse.class))
+                .collect(Collectors.toList());
+        return ResponseEntity.status(HttpStatus.OK).body(soldierResponseList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SoldierResponse> searchSoldier(@PathVariable() Long id) {
+        SoldierResponse soldierResponse = soldierService.findSoldier(id);
+        return ResponseEntity.status(HttpStatus.OK).body(soldierResponse);
+    }
+
+    @PostMapping
+    public ResponseEntity createSoldier(@RequestBody Soldier soldier) {
+        soldierService.createSoldier(soldier);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateSoldier(@PathVariable() Long id,
+                                        @RequestBody SoldierEditRequest soldierEditRequest) {
+        soldierService.updateSoldier(id, soldierEditRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteSoldier(@PathVariable Long id) {
+        soldierService.deleteSoldier(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
--- spring-api-hateoas/src/main/java/com/guilherme/springapihateoas/service/SoldierService.java
+++ spring-api-hateoas/src/main/java/com/guilherme/springapihateoas/service/SoldierService.java
@@ -7,17 +7,26 @@ import com.guilherme.springapihateoas.dto.Soldier;
 import com.guilherme.springapihateoas.entity.SoldierEntity;
 import com.guilherme.springapihateoas.repository.SoldierRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@AllArgsConstructor
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class SoldierService {
 
     private SoldierRepository soldierRepository;
     private ObjectMapper objectMapper;
+
+    public List<Soldier> listAllSoldiers(){
+        List<SoldierEntity> soldierEntityList = soldierRepository.findAll();
+        List<Soldier> streamSoldier = soldierEntityList.stream()
+                .map(it -> objectMapper.convertValue(it, Soldier.class))
+                .collect(Collectors.toList());
+        return streamSoldier;
+    }
 
     public SoldierResponse findSoldier(Long id) {
         SoldierEntity soldierEntity = soldierRepository.findById(id).orElseThrow();
@@ -39,14 +48,6 @@ public class SoldierService {
     public void deleteSoldier(Long id) {
         SoldierEntity soldierEntity = soldierRepository.findById(id).orElseThrow();
         soldierRepository.delete(soldierEntity);
-    }
-
-    public List<Soldier> listAllSoldiers(){
-        List<SoldierEntity> soldierEntityList = soldierRepository.findAll();
-        List<Soldier> streamSoldier = soldierEntityList.stream()
-                .map(it -> objectMapper.convertValue(it, Soldier.class))
-                .collect(Collectors.toList());
-        return streamSoldier;
     }
 }
 
