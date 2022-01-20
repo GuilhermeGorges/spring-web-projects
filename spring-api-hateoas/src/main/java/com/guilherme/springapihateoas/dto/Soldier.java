c3cd1f2a390bd2d8c4efede0391be1ded9f82111
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-01-20 19:11:11 -0300

    Develop Soldier Entity

--- spring-api-hateoas/src/main/java/com/guilherme/springapihateoas/dto/Soldier.java
+++ spring-api-hateoas/src/main/java/com/guilherme/springapihateoas/dto/Soldier.java
@@ -1,18 +1,25 @@
 package com.guilherme.springapihateoas.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Soldier {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String cpf;
-    private String nome;
-    private String raca;
-    private String arma;
+    private String name;
+    private String breed;
+    private String weapon;
     private String status;
 }
--- spring-api-hateoas/src/main/java/com/guilherme/springapihateoas/entity/SoldierEntity.java
+++ spring-api-hateoas/src/main/java/com/guilherme/springapihateoas/entity/SoldierEntity.java
@@ -1,4 +1,27 @@
 package com.guilherme.springapihateoas.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SoldierEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String breed;
+    private String weapon;
+    private String status;
 }
