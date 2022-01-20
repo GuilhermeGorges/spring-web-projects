93dbf1c7ff0f5f43a0bef5adf15a66ac35fd7f40
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-01-20 18:58:28 -0300

    Spring API Hateoas Init

---
+++ spring-api-hateoas/src/main/java/com/guilherme/springapihateoas/SpringApiHateoasApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.springapihateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiHateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiHateoasApplication.class, args);
+	}
+
+}
---
+++ spring-api-hateoas/src/test/java/com/guilherme/springapihateoas/SpringApiHateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.springapihateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiHateoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
