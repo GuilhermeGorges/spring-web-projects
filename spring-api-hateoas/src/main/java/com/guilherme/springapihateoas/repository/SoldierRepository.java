81a7a4036fa8b64b5cbd086c7ecd1f11c405a392
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-01-20 19:12:03 -0300

    Develop Soldier Repository

--- spring-api-hateoas/src/main/java/com/guilherme/springapihateoas/repository/SoldierRepository.java
+++ spring-api-hateoas/src/main/java/com/guilherme/springapihateoas/repository/SoldierRepository.java
@@ -1,4 +1,9 @@
 package com.guilherme.springapihateoas.repository;
 
-public interface SoldierRepository {
+import com.guilherme.springapihateoas.entity.SoldierEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SoldierRepository extends JpaRepository<SoldierEntity, Long> {
 }
