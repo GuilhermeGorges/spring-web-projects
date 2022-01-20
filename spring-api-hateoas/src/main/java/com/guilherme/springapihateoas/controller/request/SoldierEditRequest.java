ead748ed2d8f053768ea2de16e250c6b49d5e452
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-01-20 19:28:59 -0300

    Develop Soldier Req/Res

--- spring-api-hateoas/src/main/java/com/guilherme/springapihateoas/controller/request/SoldierEditRequest.java
+++ spring-api-hateoas/src/main/java/com/guilherme/springapihateoas/controller/request/SoldierEditRequest.java
@@ -1,4 +1,15 @@
 package com.guilherme.springapihateoas.controller.request;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class SoldierEditRequest {
+    private String name;
+    private String breed;
+    private String weapon;
+    private String status;
 }
--- spring-api-hateoas/src/main/java/com/guilherme/springapihateoas/controller/response/SoldierResponse.java
+++ spring-api-hateoas/src/main/java/com/guilherme/springapihateoas/controller/response/SoldierResponse.java
@@ -1,4 +1,24 @@
 package com.guilherme.springapihateoas.controller.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class SoldierResponse {
+    @Id
+    @JsonProperty("id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String breed;
+    private String weapon;
+    private String status;
 }
