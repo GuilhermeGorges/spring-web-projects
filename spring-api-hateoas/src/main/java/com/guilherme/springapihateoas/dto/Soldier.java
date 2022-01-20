7b56de80a21228c4094e1c3c809cfab2b84f877a
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-01-20 19:08:34 -0300

    Develop DTO

--- spring-api-hateoas/src/main/java/com/guilherme/springapihateoas/dto/Soldier.java
+++ spring-api-hateoas/src/main/java/com/guilherme/springapihateoas/dto/Soldier.java
@@ -1,4 +1,18 @@
 package com.guilherme.springapihateoas.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Soldier {
+
+    private Long id;
+    private String cpf;
+    private String nome;
+    private String raca;
+    private String arma;
+    private String status;
 }
